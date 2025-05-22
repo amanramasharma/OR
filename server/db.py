23e4a0a5bf37be6528635e1adc776490dd843640
Aman Sharma <amanramasharma@gmail.com>
2025-05-22 15:49:51 +0100

    Update db.py

--- server/db.py
+++ server/db.py
@@ -3,7 +3,8 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-client = MongoClient(os.getenv("MONGODB_URI"))
+MONGODB_URI = "mongodb+srv://admin:admin123@optiresume.loocmvt.mongodb.net/?retryWrites=true&w=majority&appName=optiresume"
+client = MongoClient(MONGODB_URI)
 db = client["optiresume"]
 users_collection = db["users"]
 print("Connected to MongoDB:", os.getenv("MONGODB_URI"))
